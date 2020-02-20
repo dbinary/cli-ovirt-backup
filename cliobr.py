9439833969802b5c8ed5bf8fb99b57774df87d54
Luis Pérez <luis.perez@protonmail.com>
2020-02-19 19:05:44 -0600

    adding new command group and backup and restore command

--- cliobr.py
+++ cliobr.py
@@ -1,8 +1,23 @@
 import click
 
-@click.command()
-def hello():
-    click.echo('hello World!')
 
-if __name__ == '__main__':
-    hello()
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.argument('vmname')
+@click.option(
+    '--username', '-u', envvar='OVIRTUSER', help='username for oVirt API'
+)
+@click.option(
+    '--password', '-p', envvar='OVIRTPASS', help='password for oVirt user'
+)
+def backup(username, password, vmname):
+    click.echo(username)
+
+
+@cli.command()
+def restore():
+    click.echo('restore')
