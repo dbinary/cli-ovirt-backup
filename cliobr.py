0588e51b0168176cce9fa1482d08f7d0a0b0b8d2
Luis Pérez <luis.perez@protonmail.com>
2020-02-20 10:58:58 -0600

    adding options to backup

--- cliobr.py
+++ cliobr.py
@@ -1,4 +1,11 @@
+import datetime
+import os
 import click
+import logging
+import ovirtsdk4 as sdk
+import ovirtsdk4.types as types
+
+logging.basicConfig(level=logging.DEBUG, filename='example.log')
 
 
 @click.group()
@@ -9,15 +16,37 @@ def cli():
 @cli.command()
 @click.argument('vmname')
 @click.option(
+    '--username', '-u', envvar='OVIRTUSER', default='admin@internal', show_default=True, help='username for oVirt API'
+)
+@click.option(
+    '--password', '-p', envvar='OVIRTPASS', required=True, help='password for oVirt user'
+)
+@click.option(
+    '--ca', '-c', envvar='OVIRTCA', help='path for ca certificate of Manager'
+)
+@click.option(
+    '--url', '-U', envvar='OVIRTURL', required=True, help='url for oVirt API'
+)
+@click.option('--debug', is_flag=True, default=False, help='debug mode')
+def backup(username, password, ca, vmname, url, debug):
+    connection = sdk.Connection(
+        url=url,
+        username=username,
+        password=password,
+        ca_file=ca,
+        debug=debug,
+        log=logging.getLogger(),
+    )
+    logging.info('Connected to the server.')
+
+
+@cli.command()
+@click.argument('vmname')
+@click.option(
     '--username', '-u', envvar='OVIRTUSER', help='username for oVirt API'
 )
 @click.option(
     '--password', '-p', envvar='OVIRTPASS', help='password for oVirt user'
 )
-def backup(username, password, vmname):
-    click.echo(username)
-
-
-@cli.command()
-def restore():
-    click.echo('restore')
+def restore(username, password, vmname):
+    click.echo('{} {} {}'.format(username, password, vmname))
