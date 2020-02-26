cbdcff5c31772c07849d2313b4b89184af57f699
Luis Pérez <luis.perez@protonmail.com>
2020-02-26 15:08:59 -0600

    adding description and license

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='cliobr',
     version='0.1',
+    description='Script for backup and restore virtual machines in oVirt/RHV environment',
     py_modules=['cliobr'],
+    license='MIT',
     install_requires=[
         'Click',
         'ovirt-engine-sdk-python'
